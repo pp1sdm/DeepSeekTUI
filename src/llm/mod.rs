18ad3b959e1a42ce08c690561a0c5f779776ae37
zc050417 <zc050417@qq.com>
2026-05-19 17:43:52 +0800

    初始化

---
+++ src/llm/mod.rs
@@ -0,0 +1,48 @@
+use reqwest::Client;
+use serde_json::json;
+use std::{ env, time::Duration };
+use dotenvy::dotenv;
+use anyhow::{Result, anyhow};
+
+pub async fn chat(prompt: &str) -> Result<serde_json::Value> {
+    // 加载环境变量
+    dotenv().ok();
+    
+    // 获取环境变量
+    let api_key = env::var("DEEPSEEK_KEY")?;
+    
+    // 自定义客户端
+    let client = Client::builder().timeout(Duration::from_secs(60)).build()?;
+    
+    // 发送请求
+    let res = client
+        .post("https://api.deepseek.com/v1/chat/completions")
+        .header("Authorization", format!("Bearer {}", api_key))
+        .json(&json!({
+            "model": "deepseek-v4-flash",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        }))
+        .send()
+        .await?;
+    
+    // 返回的状态
+    let status = res.status();
+    
+     // 解析结果
+    let text = res.text().await?;
+    
+    // 文本序列化
+    let json: serde_json::Value = serde_json::from_str(&text)?;
+    
+    // 返回
+    if !status.is_success() {
+        return Err(anyhow!("HTTP {}: {}", status, text));
+    }
+
+    Ok(json)
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use serde_json;
+
+mod llm;
+
+#[tokio::main]
+async fn main() {
+    let res = llm::chat("你的名字是什么").await;
+    match res {
+        Ok(text) => {
+            let json = serde_json::json!({
+                "result": text
+            });
+
+            println!("{}", serde_json::to_string_pretty(&json).unwrap());
+        }
+        Err(e) => {
+            let json = serde_json::json!({
+                "error": e.to_string()
+            });
+
+            println!("{}", serde_json::to_string_pretty(&json).unwrap());
+        }
+    }
+}
